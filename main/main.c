9c66ed14ec739e96d47263c2604bed8602cd6b93
saurabh kadam <ssk3@somaiya.edu>
2024-06-10 14:42:33 +0530

    Add code for the PWM generator

--- main/main.c
+++ main/main.c
@@ -20,55 +20,35 @@ void app_main(void)
 {
     printf("Hello world \n");
 
-    // zero-initialize the config structure.
-    gpio_config_t io_conf = {};
-
+    gpio_config_t io_conf = {};                 // zero-initialize the config structure.
     io_conf.intr_type = GPIO_PIN_INTR_DISABLE;  // disable interrupt
     io_conf.mode = GPIO_MODE_OUTPUT;            // set as output mode
     io_conf.pin_bit_mask = GPIO_OUTPUT_PIN_SEL; // bit mask of the pins that you want to set,e.g.GPIO18/19
     io_conf.pull_down_en = GPIO_PULLUP_DISABLE; // disable pull-down mode
     io_conf.pull_up_en = GPIO_PULLDOWN_DISABLE; // disable pull-up mode
+    gpio_config(&io_conf);                      // configure GPIO with the given settings
 
-    gpio_config(&io_conf); // configure GPIO with the given settings
+    ledc_timer_config_t PWM_timer = {};            // zero-initialize the config structure for the timer used for the PWM.
+    PWM_timer.speed_mode = LEDC_LOW_SPEED_MODE;    // Driver in low speed mode
+    PWM_timer.duty_resolution = LEDC_TIMER_10_BIT; // Set the resolution of the timer to 10BIT
+    PWM_timer.timer_num = LEDC_TIMER_0;            // Used timer zero
+    PWM_timer.freq_hz = PWM_FREQUENCY_1KHZ;        // Set frequency to 1kHZ
+    PWM_timer.clk_cfg = LEDC_AUTO_CLK;
+    ledc_timer_config(&PWM_timer);
 
-    ledc_timer_config_t timer = {};
-    timer.speed_mode = LEDC_LOW_SPEED_MODE;
-    timer.duty_resolution = LEDC_TIMER_10_BIT;
-    timer.timer_num = LEDC_TIMER_0;
-    timer.freq_hz = 5000;
-    timer.clk_cfg = LEDC_AUTO_CLK;
-    ledc_timer_config(&timer);
-
-    ledc_channel_config_t channel = {};
-    channel.gpio_num = GPIO_NUM_2;
-    channel.speed_mode = LEDC_LOW_SPEED_MODE;
-    channel.channel = LEDC_CHANNEL_0;
-    channel.timer_sel = LEDC_TIMER_0;
-    channel.duty = 0;
-    channel.hpoint = 0;
-    ledc_channel_config(&channel);
+    ledc_channel_config_t PWM_channel_for_stepper_motor = {}; // zero-initialize the config structure for PWM channel.
+    PWM_channel_for_stepper_motor.gpio_num = STEPPER_MOTOR_PUL_PIN;
+    PWM_channel_for_stepper_motor.speed_mode = LEDC_LOW_SPEED_MODE;
+    PWM_channel_for_stepper_motor.channel = LEDC_CHANNEL_0;
+    PWM_channel_for_stepper_motor.timer_sel = LEDC_TIMER_0;
+    PWM_channel_for_stepper_motor.duty = 0;
+    PWM_channel_for_stepper_motor.hpoint = 0;
+    ledc_channel_config(&PWM_channel_for_stepper_motor);
 
     ledc_fade_func_install(0);
 
-    while (1)
-    {
+    gpio_set_level(STEPPER_MOTOR_EN_PIN, SET_GPIO_LEVEL_LOW);
+    gpio_set_level(STEPPER_MOTOR_DIR_PIN, SET_GPIO_LEVEL_HIGH);
 
-        for (int i = 0; i < 1024; i++)
-        {
-            ledc_set_duty_and_update(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, i, 0);
-            vTaskDelay(1000 / portTICK_PERIOD_MS);
-        }
-
-        gpio_set_level(STEPPER_MOTOR_EN_PIN, SET_GPIO_LEVEL_HIGH);
-        gpio_set_level(STEPPER_MOTOR_DIR_PIN, SET_GPIO_LEVEL_HIGH);
-        gpio_set_level(STEPPER_MOTOR_PUL_PIN, SET_GPIO_LEVEL_HIGH);
-
-        vTaskDelay(1000 / portTICK_RATE_MS);
-
-        gpio_set_level(STEPPER_MOTOR_EN_PIN, SET_GPIO_LEVEL_LOW);
-        gpio_set_level(STEPPER_MOTOR_DIR_PIN, SET_GPIO_LEVEL_LOW);
-        gpio_set_level(STEPPER_MOTOR_PUL_PIN, SET_GPIO_LEVEL_LOW);
-
-        vTaskDelay(1000 / portTICK_RATE_MS);
-    }
+    ledc_set_duty_and_update(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, 512, 0);
 }
