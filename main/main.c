ae2d3e8408dda0e9d7d15cfd8b2be6ed112c9c37
saurabh kadam <ssk3@somaiya.edu>
2024-06-10 11:16:22 +0530

    Add code for the generating PWM signal

--- main/main.c
+++ main/main.c
@@ -31,8 +31,33 @@ void app_main(void)
 
     gpio_config(&io_conf); // configure GPIO with the given settings
 
+    ledc_timer_config_t timer = {};
+    timer.speed_mode = LEDC_LOW_SPEED_MODE;
+    timer.duty_resolution = LEDC_TIMER_10_BIT;
+    timer.timer_num = LEDC_TIMER_0;
+    timer.freq_hz = 5000;
+    timer.clk_cfg = LEDC_AUTO_CLK;
+    ledc_timer_config(&timer);
+
+    ledc_channel_config_t channel = {};
+    channel.gpio_num = GPIO_NUM_2;
+    channel.speed_mode = LEDC_LOW_SPEED_MODE;
+    channel.channel = LEDC_CHANNEL_0;
+    channel.timer_sel = LEDC_TIMER_0;
+    channel.duty = 0;
+    channel.hpoint = 0;
+    ledc_channel_config(&channel);
+
+    ledc_fade_func_install(0);
+
     while (1)
     {
+
+        for (int i = 0; i < 1024; i++)
+        {
+            ledc_set_duty_and_update(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, i, 0);
+            vTaskDelay(1000 / portTICK_PERIOD_MS);
+        }
 
         gpio_set_level(STEPPER_MOTOR_EN_PIN, SET_GPIO_LEVEL_HIGH);
         gpio_set_level(STEPPER_MOTOR_DIR_PIN, SET_GPIO_LEVEL_HIGH);
--- main/main.h
+++ main/main.h
@@ -24,12 +24,13 @@
 #include "freertos/task.h"
 #include "freertos/queue.h"
 #include "driver/gpio.h"
+#include "driver/ledc.h"
 
 #define SET_GPIO_LEVEL_HIGH 0x01
 #define SET_GPIO_LEVEL_LOW 0x00
 
 #define STEPPER_MOTOR_EN_PIN GPIO_NUM_15
-#define STEPPER_MOTOR_DIR_PIN GPIO_NUM_2
+#define STEPPER_MOTOR_DIR_PIN GPIO_NUM_4
 #define STEPPER_MOTOR_PUL_PIN GPIO_NUM_0
 
 #define GPIO_OUTPUT_PIN_SEL ((1ULL << STEPPER_MOTOR_EN_PIN) | (1ULL << STEPPER_MOTOR_DIR_PIN) | (1ULL << STEPPER_MOTOR_PUL_PIN))
