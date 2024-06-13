f419d8c54389e3c85300cb7314f55bcb98b4f78f
saurabh kadam <ssk3@somaiya.edu>
2024-06-13 16:58:00 +0530

    Add all the include all the dependency for the console application

--- main/console.h
+++ main/console.h
@@ -1,8 +1,8 @@
 /*H**********************************************************************
- * FILENAME :        main.h             DESIGN REF: NA
+ * FILENAME :        console.h             DESIGN REF: NA
  *
  * DESCRIPTION :
- *       main.h file for the stepper motor example.
+ *       console.h file for the stepper motor example.
  *
  * NOTES :
  *       These functions are a part of the FM suite;
@@ -11,10 +11,23 @@
  *       Copyright: All rights reserved.
  *
  * AUTHOR     :  Saurabh kadam.
- * START DATE :  08 Jun 2024
+ * START DATE :  13 Jun 2024
  *H*/
 
 #ifndef CONSOLE_H
 #define CONSOLE_H
+
+#include <stdio.h>
+#include <string.h>
+#include "esp_system.h"
+#include "esp_log.h"
+#include "esp_console.h"
+#include "esp_vfs_dev.h"
+#include "driver/uart.h"
+#include "linenoise/linenoise.h"
+#include "argtable3/argtable3.h"
+#include "esp_vfs_fat.h"
+#include "nvs.h"
+#include "nvs_flash.h"
 
 #endif // CONSOLE_H
