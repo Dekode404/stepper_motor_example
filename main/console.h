9db21ca4034449e1d30e772df573663075ecf081
saurabh kadam <ssk3@somaiya.edu>
2024-06-13 16:55:38 +0530

    Add console.c & console.h file for impanation of the console command application

---
+++ main/console.h
@@ -0,0 +1,20 @@
+/*H**********************************************************************
+ * FILENAME :        main.h             DESIGN REF: NA
+ *
+ * DESCRIPTION :
+ *       main.h file for the stepper motor example.
+ *
+ * NOTES :
+ *       These functions are a part of the FM suite;
+ *       See IMS FM0121 for detailed description.
+ *
+ *       Copyright: All rights reserved.
+ *
+ * AUTHOR     :  Saurabh kadam.
+ * START DATE :  08 Jun 2024
+ *H*/
+
+#ifndef CONSOLE_H
+#define CONSOLE_H
+
+#endif // CONSOLE_H
